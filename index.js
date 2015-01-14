ed8578434a2942881c83bb81e32590b483d6d795
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-13 22:26:11 -0800

    Rewrite example in Kuala Lumpur

--- index.js
+++ index.js
@@ -2,31 +2,47 @@ var ss = require('simple-statistics');
 var inside = require('turf-inside');
 
 /**
-* Takes a set of polygons, a set of points, and tag polygons with the sum
-* of point property values contained within.
-*
-* @module turf/max
-* @param {FeatureCollection} polygons a FeatureCollection of {@link Polygon} features
-* @param {FeatureCollection} points a FeatureCollection of {@link Point} features
-* @param {string} inField the field in input data to analyze
-* @param {string} outField the field in which to store results
-* @return {FeatureCollection} a FeatureCollection of {@link Polygon} features
-* with properties listed as `outField` values in `aggregations`
-* @example
-* var polygons = turf.featurecollection([
-*   turf.polygon([[[0,0],[10,0],[10,10],[0,10],[0,0]]]),
-*   turf.polygon([[[10,0],[20,10],[20,20], [20,0]]])]);
-* var points = turf.featurecollection([
-*   turf.point(5,5, {population: 200}),
-*   turf.point(1,3, {population: 600}),
-*   turf.point(14,2, {population: 100}),
-*   turf.point(13,1, {population: 200}),
-*   turf.point(19,7, {population: 300})]);
-* var aggregated = turf.max(polygons, points, 'population', 'max');
-* //=polygons
-* //=points
-* //=aggregated
-*/
+ * Calculates the maximum value of a field for points within a set of polygons.
+ *
+ * @module turf/max
+ * @param {FeatureCollection} polygons a FeatureCollection of {@link Polygon} features
+ * @param {FeatureCollection} points a FeatureCollection of {@link Point} features
+ * @param {string} inField the field in input data to analyze
+ * @param {string} outField the field in which to store results
+ * @return {FeatureCollection} a FeatureCollection of {@link Polygon} features
+ * with properties listed as `outField` values in `aggregations`
+ * @example
+ * var polygons = turf.featurecollection([
+ *   turf.polygon([[
+ *     [101.551437, 3.150114],
+ *     [101.551437, 3.250208],
+ *     [101.742324, 3.250208],
+ *     [101.742324, 3.150114],
+ *     [101.551437, 3.150114]
+ *   ]]),
+ *   turf.polygon([[
+ *     [101.659927, 3.011612],
+ *     [101.659927, 3.143944],
+ *     [101.913986, 3.143944],
+ *     [101.913986, 3.011612],
+ *     [101.659927, 3.011612]
+ *   ]])
+ * ]);
+ * var points = turf.featurecollection([
+ *   turf.point(101.56105, 3.213874, {population: 200}),
+ *   turf.point(101.709365, 3.211817, {population: 600}),
+ *   turf.point(101.645507, 3.169311, {population: 100}),
+ *   turf.point(101.708679, 3.071266, {population: 200}),
+ *   turf.point(101.826782, 3.081551, {population: 300})]);
+ *
+ * var aggregated = turf.max(
+ *   polygons, points, 'population', 'max');
+ *
+ * var result = turf.featurecollection(
+ *   points.features.concat(aggregated.features));
+ *
+ * //=result
+ */
 module.exports = function(polyFC, ptFC, inField, outField){
   polyFC.features.forEach(function(poly){
     if(!poly.properties){
