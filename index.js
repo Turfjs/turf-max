6aeb1fd050767fe9984aedbbbe08e89b6fdc327e
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-26 12:46:52 -0800

    tighten description language

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var inside = require('turf-inside');
 
 /**
- * Calculates the maximum value of a field for points within a set of polygons.
+ * Calculates the maximum value of a field for a set of {@link Point|points} within a set of {@link Polygon|polygons}.
  *
  * @module turf/max
  * @category aggregation
